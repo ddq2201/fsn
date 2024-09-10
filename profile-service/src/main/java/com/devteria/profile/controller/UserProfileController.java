288ef7dae3aca97c8df289f4da20bda1b21e250b
ddq2201 <datdongquoc220102@gmail.com>
2024-09-10 17:06:21 +0700

    firstCommit

---
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -0,0 +1,40 @@
+package com.devteria.profile.controller;
+
+import com.devteria.profile.dto.request.ProfileCreationRequest;
+import com.devteria.profile.dto.response.ApiResponse;
+import com.devteria.profile.dto.response.UserProfileResponse;
+import com.devteria.profile.service.UserProfileService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserProfileController {
+    UserProfileService userProfileService;
+
+    @GetMapping("/users/{profileId}")
+    UserProfileResponse getProfile(@PathVariable String profileId) {
+
+        return userProfileService.getProfile(profileId);
+    }
+
+    @GetMapping("/users")
+    ApiResponse<List<UserProfileResponse>> getAllProfiles() {
+        return ApiResponse.<List<UserProfileResponse>>builder()
+                .result(userProfileService.getAllProfiles())
+                .build();
+    }
+
+    @DeleteMapping("/users/delete/{profileId}")
+    public ResponseEntity<String> deleteProfile(@PathVariable String profileId) {
+        userProfileService.deleteProfile(profileId);
+        return ResponseEntity.ok().body("SUCCESS"); // Return 204 No Content on successful deletion
+    }
+}
