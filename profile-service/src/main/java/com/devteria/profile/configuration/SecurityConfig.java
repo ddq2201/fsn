28b0627a59b22fa9adf55c3383e3e235cda23b9f
ddq2201 <datdongquoc220102@gmail.com>
2024-09-11 10:32:57 +0700

    add authentication and authorization
    Signed-off-by: ddq2201 <datdongquoc220102@gmail.com>

--- profile-service/src/main/java/com/devteria/profile/configuration/SecurityConfig.java
+++ profile-service/src/main/java/com/devteria/profile/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private static final String[] PUBLIC_ENDPOINTS = {
-        "/users/registration", "/auth/token", "/auth/introspect", "/auth/logout", "/auth/refresh"
+
     };
 
     private final CustomJwtDecoder customJwtDecoder;
@@ -53,10 +53,5 @@ public class SecurityConfig {
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
 
         return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
     }
 }
